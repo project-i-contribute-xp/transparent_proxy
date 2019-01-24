f307033b8ab6d73c5f5e16e5cf24ce593d0bbe8d
wen <wen@trackview.co>
2019-01-24 16:42:00 +0800

    implement choose server script
    TEST: ./choose_server.sh

---
+++ choose_server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# ./build_shadowsocks.sh
+./generate_config.sh ./gui-config.json
+
+line=0
+for CONFIG in $(find ./config -name "config_*.json" | sort);
+do
+    echo "["$line"]"$(cat $CONFIG | jq .server)
+    line=$(($line+1))
+done
+
+read -p 'Please choose server:' server
+
+cp -f $(find ./config -name "config_*.json" | sort | sed -n $server" p") ./config.json
+
+./startup_shadowsocks.sh
